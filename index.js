38ae4d1aec757a6d91ee516befd3e37d4fcc9baa
bhavitagadara <168505737+bhavitagadara@users.noreply.github.com>
2025-05-12 17:46:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ connectToMongoDB("mongodb://127.0.0.1:27017/foodapptemp",{
   
 
   const corsOptions = {
-    origin: ' http://localhost:4200', // Allow requests from this origin
+    origin: 'http://localhost:4200', // Allow requests from this origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers 
     credentials: true 
